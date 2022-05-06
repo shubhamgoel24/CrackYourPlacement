983a6e6f135591439f90e2d872c2da60113d7928
Shubham Goel <sshubhamgoel24@gmail.com>
2022-05-06 20:05:06 +0530

    Day2

---
+++ Day2/Chocolate Distribution Problem.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> arr, long long n, long long m){
+    //code
+        if (m == 0 || n == 0)
+    		return 0;
+    	if (n < m)
+    		return -1;
+    		
+    	sort(arr.begin(), arr.end());
+    	
+    	long long int min_diff = INT_MAX;
+    	for (int i = 0; i + m - 1 < n; i++) {
+    		int diff = arr[i + m - 1] - arr[i];
+    		if (diff < min_diff)
+    			min_diff = diff;
+    	}
+    	return min_diff;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
